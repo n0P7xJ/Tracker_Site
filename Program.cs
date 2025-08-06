5fbfe0604adad376d98a2668df42e4e7e8b894e3
n0P7xJ <program79766971@gmail.com>
2025-08-06 17:08:22 +0200

    Init Project

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Firefox;
+
+class Program
+{
+    static void Main()
+    {
+        // Ініціалізація FirefoxDriver
+        using IWebDriver driver = new FirefoxDriver();
+
+        // Переходимо на Google
+        driver.Navigate().GoToUrl("https://www.google.com");
+
+        // Знаходимо поле пошуку за атрибутом name="q"
+        IWebElement searchBox = driver.FindElement(By.Name("q"));
+
+        // Вводимо текст і відправляємо форму
+        searchBox.SendKeys("Selenium C#");
+        searchBox.Submit();
+
+        // Трохи почекаємо, щоб побачити результат
+        Console.ReadLine();
+
+        // Закриваємо браузер
+        driver.Quit();
+    }
+}
